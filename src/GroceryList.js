18a78dedf171cf15bf331bfa8f32be3c0998b644
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-25 16:59:53 +0200

    Update GroceryList.js

--- src/GroceryList.js
+++ src/GroceryList.js
@@ -5,18 +5,11 @@ export class GroceryList extends Component{
     state={
         userInput: "",
         groceryList: []
-
-        // iznachalnoe sostoyanie -pystaya string, 
-        // sozdaem pystoi massiv kyda bydem dobavlyat elementy, eti dve xarakteristiki bydyt postoyanno menyatsya
     }
 
     onChangeEvent(e){
         this.setState({userInput: e})
-        
-// evemt- e- eto click, izmenenye chego-libo naprimer texta.. console.log(e.target.value)event.target.value ili e.target.value- to chto pozvolyaet nam poluchit 
-// dostup k tomy chto pechataet polzovatel, cherez setstate menyaem user input v zavisimosti ot togo chto pishet polzovatel
-         
-    }
+  }
 
     addItem(input){
         if (input===""){
@@ -52,8 +45,6 @@ deleteItem(){
                 placeholder="What do you want to buy?"
                 onChange={(e) => {this.onChangeEvent(e.target.value)}} 
                 value={this.state.userInput}/>
-            {/* //1 poluchaem dostup k tomy chto pishet user, 2 menyaem user input v sootvetstvii s ettim onChange et metod kotoryi otslegivaet izmeneniya sdelannye polzovatelem,
-            // value eto to chto pishet polzovatel na dannyi moment(znachenie), poluchaem dostp k tomy chto pishet polzovatel */}
             </div>
 
             <div  className="container">
@@ -62,7 +53,7 @@ deleteItem(){
 
             <ul>
                 {this.state.groceryList.map((item, index)=>(
-                /* stavim map na ves spisok govorim chto xotim poluchit dostup k kagdomy item */
+                
                 <li onClick={this.crossedWord} key={index}><img src={icon} width="40px" alt="icon"/> {item}</li>
                 ))}
             </ul>
